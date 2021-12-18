927d9d5d049727cd1886ac6a079329d23c0765c7
Stas Fomin <stas-fomin@yandex.ru>
2021-12-18 22:33:13 +0300

    first version with looping, will try to minimize/bisect

---
+++ setup-demo.bat
@@ -0,0 +1,10 @@
+#!/bin/sh
+C:\ta-buildroot\python-x86-3.7.9\python.exe -m pip install pipenv
+C:\ta-buildroot\python-x86-3.7.9\python.exe -m pipenv install
+
+
+
+
+
+
+
---
+++ test.py
@@ -0,0 +1,7 @@
+
+import numpy as np
+from sklearn.neighbors import KDTree
+import sklearn
+
+if __name__ == "__main__":
+    print(sklearn.__version__)
