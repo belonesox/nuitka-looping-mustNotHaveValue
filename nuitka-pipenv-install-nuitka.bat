a2e49123129b0e465c02fa7d759caee272860888
Stas Fomin <stas-fomin@yandex.ru>
2021-12-20 17:18:15 +0300

    the bug starts with 0.6.17.1 or 0.6.17.0

--- nuitka-pipenv-install-nuitka.bat
+++ nuitka-pipenv-install-nuitka.bat
@@ -1,6 +1,7 @@
 rem
 set PATH=C:\ta-buildroot\python-x86-3.8.10;C:\ta-buildroot\python-x86-3.7.9;%PATH%
-python.exe -m pipenv install nuitka==0.6.17.7
+python.exe -m pipenv uninstall nuitka
+python.exe -m pipenv install nuitka==0.6.17.1
 
 
 
