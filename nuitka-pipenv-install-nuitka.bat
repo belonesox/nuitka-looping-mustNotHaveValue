12942828f8952323a8cd1b7585bf0183fef58b9f
Stas Fomin <stas-fomin@yandex.ru>
2021-12-20 10:16:35 +0300

    minimizing, 6.17.7 looping

--- nuitka-pipenv-install-nuitka.bat
+++ nuitka-pipenv-install-nuitka.bat
@@ -1,7 +1,6 @@
 rem
 set PATH=C:\ta-buildroot\python-x86-3.8.10;C:\ta-buildroot\python-x86-3.7.9;%PATH%
-python.exe -m pipenv run python -m nuitka --version
-python.exe -m pipenv run python -V 
+python.exe -m pipenv install nuitka==0.6.17.7
 
 
 
--- setup-demo.bat
+++ setup-demo.bat
@@ -1,9 +1,10 @@
 #!/bin/sh
-set PATH=C:\ta-buildroot\python-x86-3.7.9;C:\ta-buildroot\python-x86-3.8.10;%PATH%
+set PATH=C:\ta-buildroot\python-x86-3.8.10;C:\ta-buildroot\python-x86-3.7.9;%PATH%
 python.exe -m pip install pipenv
+del /Q Pipfile.lock
 python.exe -m pipenv --rm
 python.exe -m pipenv install 
-python.exe -m nuitka --version
+python.exe -m pipenv run python -m nuitka --version
 
 
 
